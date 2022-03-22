9f689b8d608cb52dbf0a7a70a54c1167f2860aff
Dmytro Davydenko <dmytro.davydenko@hp.com>
2022-03-22 09:34:02 +0200

    Initial custom publisher

--- MoreCombine/Sources/MoreCombine/MoreCombine.swift
+++ MoreCombine/Sources/MoreCombine/MoreCombine.swift
@@ -2,23 +2,53 @@ import Combine
 
 @available(iOS 13.0, *)
 public struct FibonacchiPublisher: Publisher {
+	
 	public typealias Output = Int
 	
 	public typealias Failure = Never
-
-	@available(iOS 13.0, *)
-	public func receive<S>(subscriber: S) where S : Subscriber, Never == S.Failure, Int == S.Input {
-		
-	}
-	
-	
-}
-
-public struct MoreCombine {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
+    var limit: Subscribers.Demand
+    
+    public init(limit: Subscribers.Demand) {
+        self.limit = limit
     }
     
-    
+	@available(iOS 13.0, *)
+	public func receive<S>(subscriber: S) where S : Subscriber, Never == S.Failure, Int == S.Input {
+        let subscription = FibonacciSubscription(subscriber: subscriber, limit: self.limit)
+		subscriber.receive(subscription: subscription)
+	}
+	
+	@available(iOS 13.0, *)
+    class FibonacciSubscription<S: Subscriber>: Subscription where S.Input == Int {
+        var current = 1
+        var previous = 0
+        var limit: Subscribers.Demand
+        
+        var subscriber: S?
+        
+        init(subscriber: S, limit: Subscribers.Demand) {
+            self.subscriber = subscriber
+            self.limit = limit
+        }
+        
+        func request(_ demand: Subscribers.Demand) {
+			guard var amount = (demand > self.limit ? self.limit : demand).max else {
+				subscriber?.receive(completion: .finished)
+				return
+			}
+			
+			while let subscriber = self.subscriber, amount > 0 {
+				let now = current + previous
+				let newDemand = subscriber.receive(current)
+				Swift.print("\(newDemand)")
+				previous = current
+				current = now
+				amount -= 1
+			}
+        }
+        
+        func cancel() {
+            self.subscriber = nil
+        }
+    }
 }
--- MoreCombine/Tests/MoreCombineTests/MoreCombineTests.swift
+++ MoreCombine/Tests/MoreCombineTests/MoreCombineTests.swift
@@ -1,11 +1,24 @@
 import XCTest
+import Combine
 @testable import MoreCombine
 
+@available(iOS 13.0, *)
 final class MoreCombineTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(MoreCombine().text, "Hello, World!")
-    }
+	var cancellable: AnyCancellable?
+	
+	func testWithUnlimitedSubscriber() throws {
+	
+		var comleted = false
+		var data = [Int]()
+		cancellable = FibonacchiPublisher(limit: Subscribers.Demand.max(7)).sink { completion in
+			XCTAssert(completion == .finished)
+			comleted = true
+		} receiveValue: {
+			data.append($0)
+		}
+		
+		XCTAssertTrue(comleted)
+		XCTAssertEqual(data, [1, 1, 2, 3, 5, 8, 13])
+		cancellable = nil
+	}
 }
